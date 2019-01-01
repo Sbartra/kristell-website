7b4a7f9c94ac5b436350d848be626842835d8d6f
Sbartra <segundobartra@gmail.com#>
2019-01-01 20:23:00 +0000

    website code

---
+++ main.js
@@ -0,0 +1,14 @@
+function myFunction(imgs) {
+    var expandImg = document.getElementById("expandedImg");
+    var imgText = document.getElementById("imgtext");
+    expandImg.src = imgs.src;
+    imgText.innerHTML = imgs.alt;
+    expandImg.parentElement.style.display = "block";
+}
+
+function w3_open() {
+    var x = document.getElementById("mySidebar");
+    x.style.width = "300px";
+    x.style.paddingTop = "10%";
+    x.style.display = "block";
+}
