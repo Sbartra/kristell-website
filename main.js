5dfa45f5f7bb61dfbf74e902ef2d10b71a6aa24b
Segundo Bartra <42465258+Sbartra@users.noreply.github.com>
2019-02-01 09:37:01 +0000

    Update main.js

--- main.js
+++ main.js
@@ -6,6 +6,11 @@ function myFunction(imgs) {
     expandImg.parentElement.style.display = "block";
 }
 
+// Close side navigation
+function w3_close() {
+  document.getElementById("mySidebar").style.display = "none";
+}
+
 function w3_open() {
     var x = document.getElementById("mySidebar");
     x.style.width = "300px";
